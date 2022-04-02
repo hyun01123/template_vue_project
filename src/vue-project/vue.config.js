6f49dc31f705d0b315d7775dcdc237b2d9f322de
kimhyunkyung <hyun01123@naver.com>
2022-04-02 17:27:31 +0900

    spring

---
+++ src/vue-project/vue.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  outputDir : path.resolve(__dirname, "../" + "main/resources/static"),
+  devServer : {
+    proxy : {
+      '/api' : {
+        target : 'http://localhost:9000',
+        ws : true,
+        changeOrigin : true
+      },
+    },
+    overlay: false,
+  },
+  chainWebpack : config => {
+    config.plugins.delete('prefetch');
+  }
+}
