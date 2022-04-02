1b406362bdd319ca6cc8c9c45dcc5242fd3e2d93
kimhyunkyung <hyun01123@naver.com>
2022-04-02 16:53:40 +0900

    spring

---
+++ src/test/java/com/example/vue3_template/Vue3TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vue3_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vue3TemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
