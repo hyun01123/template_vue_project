1d89e4cada70f9e76170d6d1ea806c3a2477b8d5
kimhyunkyung <hyun01123@naver.com>
2022-04-02 16:08:59 +0900

    default spring project

---
+++ src/test/java/com/example/template_vue_project/TemplateVueProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.template_vue_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateVueProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
